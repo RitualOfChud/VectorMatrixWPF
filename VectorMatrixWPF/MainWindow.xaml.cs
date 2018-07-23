781c9e9de28aa2c079477aed708b6a9cb734d0f9
ritualofchud <ritualofchud@gmail.com>
2018-07-23 07:16:11 +0200

    Lay down basic XY graph - set up the X and Y axes - X & Y axes can be responsive to resizing (if resizing allowed)

--- VectorMatrixWPF/MainWindow.xaml.cs
+++ VectorMatrixWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,60 @@ namespace VectorMatrixWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        public double _canvasHeight;
+        public double CanvasHeight
+        {
+            get { return _canvasHeight; }
+            set { NotifyPropertyChanged(); _canvasHeight = value; }
+        }
+
+        public double _canvasWidth;
+        public double CanvasWidth
+        {
+            get { return _canvasWidth; }
+            set { NotifyPropertyChanged(); _canvasWidth = value; }
+        }
+
+        public double _canvasHalfWidth;
+        public double CanvasHalfWidth
+        {
+            get { return _canvasHalfWidth; }
+            set { NotifyPropertyChanged(); _canvasHalfWidth = value; }
+        }
+
+        public double _canvasHalfHeight;
+        public double CanvasHalfHeight
+        {
+            get { return _canvasHalfHeight; }
+            set { NotifyPropertyChanged(); _canvasHalfHeight = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            // resize events
+            Loaded += delegate { ResizeCanvas(new object(), new RoutedEventArgs()); };
+            StateChanged += delegate { ResizeCanvas(new object(), new RoutedEventArgs()); };
+            Chart_Canvas.SizeChanged += ResizeCanvas;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void ResizeCanvas(object sender, RoutedEventArgs e)
+        {
+            CanvasHeight = Chart_Canvas.ActualHeight;
+            CanvasWidth = Chart_Canvas.ActualWidth;
+            CanvasHalfWidth = CanvasWidth / 2;
+            CanvasHalfHeight = CanvasHeight / 2;
+            Console.WriteLine("");
+        }
+
     }
 }
---
+++ VectorMatrixWPF/Models/SquareGrid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorMatrixWPF.Models
+{
+    public  class SquareGrid
+    {
+
+        private readonly int _size;
+
+
+
+    }
+}
